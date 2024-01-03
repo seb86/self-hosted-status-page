e07705a78b6c8060cde562e05f63f6bf8bd9d80d
Sébastien Dumont <mailme@sebastiendumont.com>
2024-01-03 15:25:41 +0100

    Added services.php

---
+++ services.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Define the API and webpage details.
+ *
+ * example:
+ *
+ * 'Google' => [
+ *      'name' => 'Google', // Name of service, must match array key.
+ *      'url' => 'https://google.com', // URL
+ *      'method' => 'GET', // GET or POST
+ *      'data' => null, // Add an array of data if posting data.
+ *  ],
+ *
+ */
+$services = [
+    'Google' => [
+        'name' => 'Google',
+        'url' => 'https://google.com',
+        'method' => 'GET',
+        'data' => null,
+    ],
+];
